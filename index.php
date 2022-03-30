094d641aedd57be7c87fe1291914d15a116444f2
Axurliz <axurlizmail@gmail.com>
2022-03-30 23:21:11 +0200

    .

---
+++ index.php
@@ -0,0 +1,89 @@
+<html>
+  <head>
+    <title>Axurliz | Home</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+  </head>
+  
+  <body>
+    <main>
+      <section class="glass">
+        <div class="dashboard">
+          <div class="user">
+            <img src="../img/avatar.png" alt="Avatar">
+            <h3>Axurliz Aarstad Pedersen</h3>
+            <p>Pro Member</p>
+          </div>
+
+          <div class="links">
+            <div class="link">
+              <img src="../img/twitch.png" alt="Twitch">
+              <h2>Stream</h2>
+            </div>
+
+            <div class="link">
+              <img src="../img/steam.png" alt="Twitch">
+              <h2>Steam</h2>
+            </div>
+            <div class="link">
+              <img src="../img/upcoming.png" alt="Twitch">
+              <h2>New</h2>
+            </div>
+
+            <div class="link">
+              <img src="../img/library.png" alt="Twitch">
+              <h2>Library</h2>
+            </div>
+
+            <div class="pro">
+              <h2>Join pro for free games.</h2>
+              <img src="../img/controller.png" alt="Controller">
+            </div>
+          </div>
+        </div>
+
+        <div class="games">
+          <div class="status">
+            <h1>Active Games</h1>
+            <input type="text">
+          </div>
+
+          <div class="cards">
+            <div class="card">
+              <img src="../img/cardplaceholder.png" alt="Game">
+              <div class="card-info">
+                <h2>Game Name</h2>
+                <p>Game Version</p>
+                <div class="progress"></div>
+              </div>
+              <h2 class="percentage">60%</h2>
+            </div>
+
+            <div class="card">
+              <img src="../img/cardplaceholder.png" alt="Game">
+              <div class="card-info">
+                <h2>Game Name</h2>
+                <p>Game Version</p>
+                <div class="progress"></div>
+              </div>
+              <h2 class="percentage">30%</h2>
+            </div>
+
+            <div class="card">
+              <img src="../img/cardplaceholder.png" alt="Game">
+              <div class="card-info">
+                <h2>Game Name</h2>
+                <p>Game Version</p>
+                <div class="progress"></div>
+              </div>
+              <h2 class="percentage">72%</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <div class="circle1"></div>
+    <div class="circle2"></div>
+  </body>
+</html>
