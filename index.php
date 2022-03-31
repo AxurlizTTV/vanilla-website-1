8f4a45e39e502aaedb9de8e67b66dadedce79533
Axurliz <axurlizmail@gmail.com>
2022-03-31 15:05:10 +0200

    Changelog Removed all unused folders. Changed most pixel values to vh values. Added different size progress bars for each game card.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <title>Axurliz | Home</title>
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="/main.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
   </head>
   
@@ -10,34 +10,34 @@
       <section class="glass">
         <div class="dashboard">
           <div class="user">
-            <img src="../img/avatar.png" alt="Avatar">
+            <img src="/img/avatar.png" alt="Avatar">
             <h3>Axurliz Aarstad Pedersen</h3>
             <p>Pro Member</p>
           </div>
 
           <div class="links">
             <div class="link">
-              <img src="../img/twitch.png" alt="Twitch">
+              <img src="/img/twitch.png" alt="Twitch">
               <h2>Stream</h2>
             </div>
 
             <div class="link">
-              <img src="../img/steam.png" alt="Twitch">
+              <img src="/img/steam.png" alt="Twitch">
               <h2>Steam</h2>
             </div>
             <div class="link">
-              <img src="../img/upcoming.png" alt="Twitch">
+              <img src="/img/upcoming.png" alt="Twitch">
               <h2>New</h2>
             </div>
 
             <div class="link">
-              <img src="../img/library.png" alt="Twitch">
+              <img src="/img/library.png" alt="Twitch">
               <h2>Library</h2>
             </div>
 
             <div class="pro">
               <h2>Join pro for free games.</h2>
-              <img src="../img/controller.png" alt="Controller">
+              <img src="/img/controller.png" alt="Controller">
             </div>
           </div>
         </div>
@@ -50,31 +50,31 @@
 
           <div class="cards">
             <div class="card">
-              <img src="../img/cardplaceholder.png" alt="Game">
+              <img src="/img/cardplaceholder.png" alt="Game">
               <div class="card-info">
                 <h2>Game Name</h2>
                 <p>Game Version</p>
-                <div class="progress"></div>
+                <div class="progress1"></div>
               </div>
               <h2 class="percentage">60%</h2>
             </div>
 
             <div class="card">
-              <img src="../img/cardplaceholder.png" alt="Game">
+              <img src="/img/cardplaceholder.png" alt="Game">
               <div class="card-info">
                 <h2>Game Name</h2>
                 <p>Game Version</p>
-                <div class="progress"></div>
+                <div class="progress2"></div>
               </div>
-              <h2 class="percentage">30%</h2>
+              <h2 class="percentage">38%</h2>
             </div>
 
             <div class="card">
-              <img src="../img/cardplaceholder.png" alt="Game">
+              <img src="/img/cardplaceholder.png" alt="Game">
               <div class="card-info">
                 <h2>Game Name</h2>
                 <p>Game Version</p>
-                <div class="progress"></div>
+                <div class="progress3"></div>
               </div>
               <h2 class="percentage">72%</h2>
             </div>
